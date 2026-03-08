d3e442bb219218b4c9cb01ae77fef7ec1d1c2f3d
hcc-developer <admin@handcraftcode.com>
2026-03-07 23:31:00 -0500

    Bump Comet to 0.7.1

--- Casks/comet.rb
+++ Casks/comet.rb
@@ -1,11 +1,11 @@
 cask "comet" do
-  version "0.5.0"
-  sha256 "229ca210a624b46d575eac09beaf9f70d5b5d3319bf651e40c35bcb404a2f31d"
+  version "0.7.1"
+  sha256 "ef6812f3f60f5fff15131d1f082badbe2f314732d20156278ab1484f9a5a5e77"
 
   url "https://storage.googleapis.com/comet-releases/releases/v#{version}/Comet_#{version}_aarch64.dmg"
   name "Comet"
   desc "MongoDB database manager"
-  homepage "https://handcraftcode.com/comet"
+  homepage "https://cometdb.dev"
 
   app "Comet.app"
 end
