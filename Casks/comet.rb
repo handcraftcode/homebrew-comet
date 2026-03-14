49816f1e271a505a588acc2edefa50fe4f78478d
hcc-developer <admin@handcraftcode.com>
2026-03-13 23:45:21 -0400

    bump comet to v1.0.4

--- Casks/comet.rb
+++ Casks/comet.rb
@@ -1,6 +1,6 @@
 cask "comet" do
-  version "1.0.3"
-  sha256 "8fa259e5fde1f25c271ef6db234967d5871da2bc167c3f58001c68751c028618"
+  version "1.0.4"
+  sha256 "0cbc3fee02a8d826084f6cbb6d3d499c037ad63f203db83bb3c93ac664aafafc"
 
   url "https://storage.googleapis.com/comet-releases/releases/v#{version}/Comet_#{version}_aarch64.dmg"
   name "Comet"
