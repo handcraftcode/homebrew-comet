8f0c604020808ea909366456ca403f5859f6cc45
hcc-developer <admin@handcraftcode.com>
2026-03-14 22:04:07 -0400

    chore: update homebrew cask to v1.1.0

--- Casks/comet.rb
+++ Casks/comet.rb
@@ -1,6 +1,6 @@
 cask "comet" do
-  version "1.0.4"
-  sha256 "0cbc3fee02a8d826084f6cbb6d3d499c037ad63f203db83bb3c93ac664aafafc"
+  version "1.1.0"
+  sha256 "552d1ab87b863224329f571ead7cce16b5ac92acba230b4d2d818453eb695d3a"
 
   url "https://storage.googleapis.com/comet-releases/releases/v#{version}/Comet_#{version}_aarch64.dmg"
   name "Comet"
